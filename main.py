d7fd761278f0be1647d89d533597b37d32e872aa
Felipe Venegas <ffvenegas6@gmail.com>
2025-11-01 12:25:12 -0300

    accepts --verbose as command line argument

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,27 +11,46 @@ client = genai.Client(api_key=api_key)
 
 def main():
 
-    if len(sys.argv) != 2:
-        print("Usage: uv run main.py <prompt>")
-        sys.exit(1)
+    verbose = False
 
-    prompt = sys.argv[1]
+    if len(sys.argv) != 2:
+        if len(sys.argv) == 3 and "--verbose" in sys.argv:
+            verbose = True
+            for arg in sys.argv:
+                if arg not in ["main.py", "--verbose"]:
+                    prompt = arg
+                    break
+        else:
+            print("Usage: uv run main.py <prompt>")
+            sys.exit(1)
+    else:
+        if sys.argv[1] != "--verbose":
+            prompt = sys.argv[1]
+        else:
+            print("Usage: uv run main.py <prompt>")
+            sys.exit(1)
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
 
     # prompt = ("Why is Boot.dev such a great place to " 
     #     "learn backend development? Use one paragraph maximum.")
 
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt
+        contents=messages,
     )
 
     text = response.text
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    response_tokens = response.usage_metadata.candidates_token_count
-
     print(text)
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: {response_tokens}")
+
+    if verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        response_tokens = response.usage_metadata.candidates_token_count
+        print(f"User prompt: {prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
 
 if __name__ == "__main__":
     main()

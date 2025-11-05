59d66a7db16935f921df82643d79570733084bdc
Felipe Venegas <ffvenegas6@gmail.com>
2025-11-05 11:27:58 -0300

    get_files_info schema tested successfuly

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 def main():
 
@@ -37,13 +44,32 @@ def main():
     # prompt = ("Why is Boot.dev such a great place to " 
     #     "learn backend development? Use one paragraph maximum.")
 
+    system_prompt = """
+    You are a helpful AI coding agent.
+
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+
+    All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
         contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions], system_instruction=system_prompt,
+        ),
     )
-
-    text = response.text
-    print(text)
+    
+    function_calls = response.function_calls
+    # print(function_calls)
+    if function_calls:
+        for function_call_part in function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+    else:
+        text = response.text
+        print(text)
 
     if verbose:
         prompt_tokens = response.usage_metadata.prompt_token_count
